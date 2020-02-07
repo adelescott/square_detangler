224de9140095ea3f9b72e1e5c8af4eff79065e70
Adele Scott <adele.frances.scott@gmail.com>
2020-02-07 17:20:42 +1100

    Improved the help descriptions of the output files.

--- src/main/scala/CliParser.scala
+++ src/main/scala/CliParser.scala
@@ -12,11 +12,11 @@ case class CliParser(arguments: Seq[String]) extends ScallopConf(arguments) {
     required = true
   )
   val outputSquareDepositsFilename: ScallopOption[String] = opt[String](
-    descr = "Coalesced Square deposits filename.",
+    descr = "Coalesced (output) Square deposits filename.",
     default = Some("consolidated_square_deposits.csv")
   )
   val outputSquareTransactionsFilename: ScallopOption[String] = opt[String](
-    descr = "Square transactions filename.",
+    descr = "Coalesced (output) Square transactions filename.",
     default = Some("consolidated_square_transactions.csv")
   )
   verify()
